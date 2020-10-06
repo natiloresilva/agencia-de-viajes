38fe4dcaf91a0a2610a841636e95d63bf08e9274
Natalia Silva <natalia.pupushi@gmail.com>
2020-10-06 12:45:14 -0300

    first commit

---
+++ server/config/database.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+require("dotenv").config({ path: "variables.env" });
+
+module.exports = new Sequelize(
+  process.env.DB_NOMBRE,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    define: {
+      timestamps: false,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    operatorAliases: false,
+  }
+);
